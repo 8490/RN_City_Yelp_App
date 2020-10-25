bded5689037090badb363ba1a74fc76e6fc7d3a3
Mert <mert.san3407@gmail.com>
2020-10-25 19:17:27 +0300

    added citylist and cityitem

--- src/pages/CityList.js
+++ src/pages/CityList.js
@@ -1,11 +1,36 @@
-import React from "react";
-import {SafeAreaView, View, Text} from "react-native";
+import axios from "axios";
+import React, {useEffect, useState} from "react";
+import {SafeAreaView, View, Text, FlatList} from "react-native";
+
+import {CityItem} from "../components"
 
 const CityList = (props) => {
+    const [cityList, setCityList] = useState([]);
+
+    // ASYNC AWAIT
+    const fetchCityData = async () => {
+        const {data} = await axios.get("http://opentable.herokuapp.com/api/cities");
+        console.log(data);
+        setCityList(data.cities);
+    }
+
+    useEffect (() => {
+        fetchCityData();
+    }, [])
+
+    const renderCities = ({item}) => <CityItem cityName={item} />
+
+    const renderSeperator = () => <View style={{borderWidth: 1, borderColor: "#e0e0e0"}} />
+
     return (
         <SafeAreaView>
             <View>
-                <Text>City List</Text>
+                <FlatList 
+                    keyExtractor={(_, index) => index.toString()}
+                    data={cityList}
+                    renderItem={renderCities}
+                    ItemSeparatorComponent={renderSeperator}
+                />
             </View>
         </SafeAreaView>
     )
